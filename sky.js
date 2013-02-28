4108b8881aefa61e1d3e0441dcd51c0d88e8225c
Dal Hundal <dal@dalhundal.com>
2013-02-28 20:44:00 +0000

    get whats on now, by fetching channel listings

--- sky.js
+++ sky.js
@@ -47,6 +47,34 @@ var Sky = function(options) {
     });
   }
 
+  var getChannelListingPart = function(channelID,date,part,fnCallback) {
+    var httpParams = {
+      host: 'tv.sky.com',
+      port: 80,
+      path: '/programme/channel/'+channelID+'/'+date+'/'+part+'.json'
+    };
+    var progs = [];
+    var req = http.request(httpParams,function(res) {
+      res.setEncoding('utf8');
+      var chunks = "";
+      res.on('data',function(chunk) { chunks = chunks+chunk; });
+      res.on('end',function() {
+        var parsed = JSON.parse(chunks);
+        for (var i in parsed.listings[channelID]) {
+          var prog = parsed.listings[channelID][i];
+          progs.push({
+            date: new Date(prog.s*1000),
+            title: prog.t,
+            description: prog.d,
+            duration: prog.m[1]/60
+          });
+        }
+        fnCallback(progs);
+      })
+    });
+    req.end();
+  }
+
   /* ==== */
 
   this.detect = function(fnCallback) {
@@ -145,6 +173,37 @@ var Sky = function(options) {
     });
   }
 
+  this.getChannelListing = function (channelID,fnCallback) {
+    var now = new Date(),
+      year = now.getFullYear(),
+      month = now.getUTCMonth() + 1,
+      date = now.getUTCDate(),
+      dateStr = year +'-'+ (month>9?month:'0'+month) +'-'+ (date>9?date:'0'+date),
+      progs = [];
+
+    getChannelListingPart(channelID,dateStr,0,function(progs0) {
+      getChannelListingPart(channelID,dateStr,1,function(progs1) {
+        getChannelListingPart(channelID,dateStr,2,function(progs2) {
+          getChannelListingPart(channelID,dateStr,3,function(progs3) {
+            progs = progs.concat(progs0,progs1,progs2,progs3);
+            fnCallback(progs);
+          });
+        });
+      });
+    });
+  }
+
+  this.whatsOn = function (channelId,date,fnCallback) {
+    this.getChannelListing(channelId,function(progs) {
+      for (var i in progs) {
+        if (progs[i].date > date) {
+          fnCallback(progs[Math.max(0,i-1)]);
+          break;
+        };
+      };
+    });
+  }
+
 };
 
 /* ==== */
@@ -155,5 +214,8 @@ var s = new Sky({
 });
 
 s.getMediaInfo(function(data) {
-  console.log('CHANNEL INFO',data)
+  s.whatsOn(data.channelID, new Date(), function(data) {
+    console.log(data);
+  });
 });
+
